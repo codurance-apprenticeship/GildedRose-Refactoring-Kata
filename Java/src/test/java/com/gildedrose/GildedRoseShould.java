4b2f370140cf0f0c41901eaa6643088464f8f74d
Andrei Bogomja <bogomja@gmail.com>
2019-08-20 10:42:22 +0100

    Adds test for reducing sell in date by one

--- Java/src/test/java/com/gildedrose/GildedRoseShould.java
+++ Java/src/test/java/com/gildedrose/GildedRoseShould.java
@@ -7,13 +7,31 @@ import static org.junit.Assert.assertEquals;
 public class GildedRoseShould {
 
     @Test
-    public void reduce_quality_of_item() {
-        Item item = new Item("+5 Dexterity Vest", 10, 20);
+    public void reduce_quality_of_regular_item_by_one_after_one_day() {
+        int initialQuality = 20;
+        Item item = new Item("regular item", 10, initialQuality);
         Item[] items = new Item[]{item};
 
         GildedRose app = new GildedRose(items);
         app.updateQuality();
 
-        assertEquals();
+        int expectedQuality = 19;
+        int actualQuality = item.quality;
+        assertEquals(expectedQuality, actualQuality);
     }
+
+    @Test
+    public void reduce_sell_in_date_by_one_after_one_day() {
+        int initialSellIn = 10;
+        Item item = new Item("regular item", initialSellIn, 20);
+        Item[] items = new Item[]{item};
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        int expectedSellIn = 9;
+        int actualSellIn = item.sellIn;
+        assertEquals(expectedSellIn, actualSellIn);
+    }
+
 }
