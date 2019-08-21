c3738cb5a0210ecc1188adffe2539930fdf7cb63
J Mir <jmir.17@gmail.com>
2019-08-21 22:11:50 +0200

    Set up combinatory test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,17 +3,26 @@ package com.gildedrose;
 import static org.junit.Assert.*;
 
 import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.Test;
 
 public class GildedRoseTest {
 
     @Test
     public void updateQuality() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        CombinationApprovals.verifyAllCombinations(
+                this::doUpdateQuality,
+                new String[] {"foo"},
+                new Integer[] {0},
+                new Integer[] {0}
+                );
+    }
+
+    private String doUpdateQuality(String itemName, int itemSellIn, int itemQuality) {
+        Item[] items = new Item[] { new Item(itemName, itemSellIn, itemQuality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        //assertEquals("foo", app.items[0].name);
-        Approvals.verify(app.items[0].name);
+        return app.items[0].toString();
     }
 
 }
