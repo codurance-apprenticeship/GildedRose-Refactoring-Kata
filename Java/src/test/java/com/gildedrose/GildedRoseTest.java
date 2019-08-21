ed7d435232512f687e298a9aedbfc958db48454c
J Mir <jmir.17@gmail.com>
2019-08-21 21:48:41 +0200

    Use approvals instead of assert for test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,18 @@ package com.gildedrose;
 
 import static org.junit.Assert.*;
 
+import org.approvaltests.Approvals;
 import org.junit.Test;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
+    public void updateQuality() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        //assertEquals("foo", app.items[0].name);
+        Approvals.verify(app.items[0].name);
     }
 
 }
