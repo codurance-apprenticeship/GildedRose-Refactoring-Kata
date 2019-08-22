e2721cbd8bb62687561b2ca5c3a87730a8dcc267
Sherlock Qiao <sherlockq@gmail.com>
2019-08-22 10:41:12 +0100

    use approve test library to make a combination test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,47 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
-
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.Test;
+
+import java.util.stream.IntStream;
 
 public class GildedRoseTest {
 
+//    @Test
+//    public void foo() {
+//        Item[] items = new Item[] { new Item("foo", 0, 0) };
+//        GildedRose app = new GildedRose(items);
+//        app.updateQuality();
+//        assertEquals("fixme", app.items[0].name);
+//    }
+
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void combinatorial_test() {
+        String[] names= {
+                GildedRose.AGED_BRIE,
+                GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT,
+                GildedRose.SULFURAS_HAND_OF_RAGNAROS,
+                "Regular Items 1",
+                "Regular Items 2"
+        };
+
+        Integer[] range = IntStream.range(-100, 101).boxed().toArray(Integer[]::new);
+        // -100 ~ 100
+        CombinationApprovals.verifyAllCombinations(
+                this::getItemResultAfterUpdate,
+                names,
+                range,
+                range
+        );
+
+    }
+
+    private String getItemResultAfterUpdate(String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        return items[0].toString();
     }
+
 
 }
